67f7517eeceae2a787ae3e8bcc214a22ed3ee15a
oleg.yashkin <oleg.yashkin@simbirsoft.com>
2022-03-29 16:48:39 +0300

    add paying, improve for kafka(bean topics etc)

--- src/main/java/com/example/yashkin/service/impl/Producer.java
+++ src/main/java/com/example/yashkin/service/impl/Producer.java
@@ -1,5 +1,6 @@
 package com.example.yashkin.service.impl;
 
+import com.example.yashkin.constants.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,19 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class Producer {
 
-    private static final String TOPIC = "user_message";
-    private static final String TOPIC_USERS = "user_message";
-
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        this.kafkaTemplate.send(TOPIC, message + "\n This message from " + userDetails.getUsername());
+        this.kafkaTemplate.send(Constants.TOPIC, message + "\n This message from " + userDetails.getUsername());
     }
 
     public void sendMessageFromService(String message) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        this.kafkaTemplate.send(TOPIC_USERS, userDetails.getUsername() + " " + message);
+        this.kafkaTemplate.send(Constants.TOPIC_USERS, userDetails.getUsername() + " " + message);
     }
 }
